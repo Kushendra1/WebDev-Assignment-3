8dd5b9e4eaf3f0fcba4da9c065025cd9566ce23f
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2021-02-28 15:58:16 -0500

    removed alerts

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let colorSelected;
 
 //Adds a Row
 function addR() {
-  alert("clicked Add Row")
+  //alert("clicked Add Row")
   const table = document.getElementById("grid");
   const rows = document.createElement("tr");
 
@@ -24,7 +24,7 @@ function addR() {
 
 //Adds a Col
 function addC() {
-  alert("Clicked Add Col")
+  //alert("Clicked Add Col")
   const table = document.getElementById("grid");
   const cols = document.createElement("tr");
 
